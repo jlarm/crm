437a50eabd590e67df497a4368dceb7b0bedeccd
jlarm <jlohr@autorisknow.com>
2024-07-09 08:54:06 -0500

    Update ReminderResource.php

--- app/Filament/Development/Resources/ReminderResource.php
+++ app/Filament/Development/Resources/ReminderResource.php
@@ -17,7 +17,7 @@ class ReminderResource extends Resource
 {
     protected static ?string $model = Reminder::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-clock';
 
     public static function form(Form $form): Form
     {
