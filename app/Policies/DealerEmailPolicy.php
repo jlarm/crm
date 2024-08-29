253e4cec4c52f33fe847f80c53e5169535b5e872
jlarm <117611704+jlarm@users.noreply.github.com>
2024-08-29 14:05:11 -0500

    Updated policy

--- app/Policies/DealerEmailPolicy.php
+++ app/Policies/DealerEmailPolicy.php
@@ -12,31 +12,26 @@ class DealerEmailPolicy
 
     public function viewAny(User $user): bool
     {
-        return $user->can('view dealer-emails');
-//        return true;
+        return $user->can('viewAny');
     }
 
     public function view(User $user, DealerEmail $dealerEmail): bool
     {
+        return $user->can('view');
     }
 
     public function create(User $user): bool
     {
+        return $user->can('create');
     }
 
     public function update(User $user, DealerEmail $dealerEmail): bool
     {
+        return $user->can('update');
     }
 
     public function delete(User $user, DealerEmail $dealerEmail): bool
     {
-    }
-
-    public function restore(User $user, DealerEmail $dealerEmail): bool
-    {
-    }
-
-    public function forceDelete(User $user, DealerEmail $dealerEmail): bool
-    {
+        return $user->can('delete');
     }
 }
