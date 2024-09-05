0022ef740337cc3e6f5bf68dd09741de26971813
jlarm <117611704+jlarm@users.noreply.github.com>
2024-09-05 08:35:02 -0500

    Customized header and footer for emails

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Route::get('/mailable', function () {
+//   $email = \App\Models\DealerEmail::first();
+//
+//   return new App\Mail\DealerEmailMail($email);
+//});
