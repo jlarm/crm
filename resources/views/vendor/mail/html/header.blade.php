05c4e82db8a6818f09c5af702bef42131ece69cb
jlarm <117611704+jlarm@users.noreply.github.com>
2024-09-09 12:30:21 -0500

    Email header logo update

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,9 +1,5 @@
 <tr>
 <td class="header">
-@if (trim($slot) === 'Laravel')
-        <img style="width:75px;height:auto;" src="{{ asset('img/logo.png') }}" alt="">
-@else
-{{ $slot }}
-@endif
+    <img style="width:75px;height:auto;" src="{{ asset('img/logo.png') }}" alt="">
 </td>
 </tr>
