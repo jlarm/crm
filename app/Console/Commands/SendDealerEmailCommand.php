cad83b10d5d32467dbd8360a6f281a7a851f220e
jlarm <117611704+jlarm@users.noreply.github.com>
2024-10-01 13:05:57 -0500

    Update SendDealerEmailCommand.php

--- app/Console/Commands/SendDealerEmailCommand.php
+++ app/Console/Commands/SendDealerEmailCommand.php
@@ -47,7 +47,7 @@ class SendDealerEmailCommand extends Command
 
             $email->update([
                 'last_sent' => now()->format('Y-m-d'),
-//                'next_send_date' => $email->frequency->value > 0 ? now()->addDays($email->frequency->value)->format('Y-m-d') : null,
+                'next_send_date' => $email->frequency->value > 0 ? now()->addDays($email->frequency->value)->format('Y-m-d') : null,
             ]);
         }
     }
