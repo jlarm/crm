eb19d1782eaa9d57c3c4b9bc3f9830da1a1125d3
jlarm <117611704+jlarm@users.noreply.github.com>
2025-05-20 10:09:38 -0500

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,6 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
+        $schedule->command('telescope:prune')->daily();
         $schedule->command('activitylog:clean')->daily();
         $schedule->command('reminder:send')->dailyAt('06:00');
         $schedule->command('dealer:send')->dailyAt('06:30');
