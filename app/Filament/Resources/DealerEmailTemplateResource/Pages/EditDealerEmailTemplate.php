f1357e1558211d8d97860bff08b379a1c2cfd7b4
jlarm <117611704+jlarm@users.noreply.github.com>
2025-09-16 14:16:11 -0500

    Updates to email template pages

--- app/Filament/Resources/DealerEmailTemplateResource/Pages/EditDealerEmailTemplate.php
+++ app/Filament/Resources/DealerEmailTemplateResource/Pages/EditDealerEmailTemplate.php
@@ -3,8 +3,15 @@
 namespace App\Filament\Resources\DealerEmailTemplateResource\Pages;
 
 use App\Filament\Resources\DealerEmailTemplateResource;
-use Filament\Actions\DeleteAction;
+use Filament\Actions;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
 use Filament\Resources\Pages\EditRecord;
+use Filament\Support\Enums\MaxWidth;
 
 class EditDealerEmailTemplate extends EditRecord
 {
@@ -13,7 +20,52 @@ class EditDealerEmailTemplate extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            DeleteAction::make(),
+            Actions\ViewAction::make(),
+            Actions\DeleteAction::make(),
         ];
+    }
+
+    public function getMaxContentWidth(): MaxWidth
+    {
+        return MaxWidth::Full;
+    }
+
+    public function hasCombinedRelationManagerTabsWithContent(): bool
+    {
+        return true;
+    }
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Section::make('Template Details')
+                    ->schema([
+                        TextInput::make('name')
+                            ->required()
+                            ->columnSpanFull(),
+                        TextInput::make('subject')
+                            ->required()
+                            ->columnSpanFull(),
+                        Select::make('pdf_attachments')
+                            ->multiple()
+                            ->relationship('pdfAttachments', 'file_name')
+                            ->preload()
+                            ->columnSpanFull()
+                            ->createOptionForm([
+                                FileUpload::make('file_path')
+                                    ->required()
+                                    ->acceptedFileTypes(['application/pdf'])
+                                    ->storeFileNamesIn('file_name')
+                                    ->directory('pdfs'),
+                            ]),
+                        RichEditor::make('body')
+                            ->hint('Use the {{contact_name}} placeholder to insert the contact\'s name.')
+                            ->hintColor('primary')
+                            ->required()
+                            ->disableToolbarButtons(['attachFiles', 'codeBlock'])
+                            ->columnSpanFull(),
+                    ]),
+            ]);
     }
 }
--- app/Filament/Resources/DealerEmailTemplateResource/Pages/ViewDealerEmailTemplate.php
+++ app/Filament/Resources/DealerEmailTemplateResource/Pages/ViewDealerEmailTemplate.php
@@ -4,69 +4,16 @@ namespace App\Filament\Resources\DealerEmailTemplateResource\Pages;
 
 use App\Filament\Resources\DealerEmailTemplateResource;
 use Filament\Actions;
-use Filament\Forms\Components\FileUpload;
-use Filament\Forms\Components\RichEditor;
-use Filament\Forms\Components\Section;
-use Filament\Forms\Components\Select;
-use Filament\Forms\Components\TextInput;
-use Filament\Forms\Form;
-use Filament\Resources\Pages\EditRecord;
-use Filament\Support\Enums\MaxWidth;
+use Filament\Resources\Pages\ViewRecord;
 
-class ViewDealerEmailTemplate extends EditRecord
+class ViewDealerEmailTemplate extends ViewRecord
 {
     protected static string $resource = DealerEmailTemplateResource::class;
 
     protected function getHeaderActions(): array
     {
         return [
-            Actions\ViewAction::make(),
-            Actions\DeleteAction::make(),
+            Actions\EditAction::make(),
         ];
-    }
-
-    public function getMaxContentWidth(): MaxWidth
-    {
-        return MaxWidth::Full;
-    }
-
-    public function hasCombinedRelationManagerTabsWithContent(): bool
-    {
-        return true;
-    }
-
-
-    public function form(Form $form): Form
-    {
-        return $form
-            ->schema([
-                Section::make('Template Details')
-                    ->schema([
-                        TextInput::make('name')
-                            ->required()
-                            ->columnSpanFull(),
-                        TextInput::make('subject')
-                            ->required()
-                            ->columnSpanFull(),
-                        Select::make('pdf_attachments')
-                            ->multiple()
-                            ->relationship('pdfAttachments', 'file_name')
-                            ->preload()
-                            ->columnSpanFull()
-                            ->createOptionForm([
-                                FileUpload::make('file_path')
-                                    ->required()
-                                    ->acceptedFileTypes(['application/pdf'])
-                                    ->storeFileNamesIn('file_name')
-                                    ->directory('pdfs'),
-                            ]),
-                        RichEditor::make('body')
-                            ->hint('Use the {{contact_name}} placeholder to insert the contact\'s name.')
-                            ->hintColor('primary')
-                            ->required()
-                            ->disableToolbarButtons(['attachFiles', 'codeBlock'])
-                            ->columnSpanFull(),
-                    ]),
-            ]);
     }
 }
